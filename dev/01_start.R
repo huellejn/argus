59a66a7582254ab1bd4b0b0e5f1e0caf2850e7d6
jhuellein <jennifer.huellein@nct-heidelberg.de>
2022-03-22 16:02:19 +0100

    Initial commit

---
+++ dev/01_start.R
@@ -0,0 +1,23 @@
+golem::fill_desc(
+  pkg_name = "argus",
+  pkg_title = "aRgus: multilevel variant visualization & advanced prediction score modeling",
+  pkg_description = "Shiny web application for assessment of pathogenicity of new genetic variants.",
+  author_first_name = "Jennifer",
+  author_last_name = "Hüllein",
+  author_email = "jennifer.huellein@nct-heidelberg.de",
+  repo_url = NULL
+)     
+golem::set_golem_options()
+usethis::use_mit_license( "Golem User" )
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_news_md( open = FALSE )
+usethis::use_git()
+golem::use_recommended_tests()
+golem::use_recommended_deps()
+golem::use_favicon()
+golem::remove_favicon()
+golem::use_utils_ui()
+golem::use_utils_server()
+rstudioapi::navigateToFile( "dev/02_dev.R" )
